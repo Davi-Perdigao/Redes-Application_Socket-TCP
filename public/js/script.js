1dc918d77c32b1ea1dff7675dda9b579f556bb63
Davi Perdigão <davivcperdigao@gmail.com>
2022-10-24 15:59:27 -0300

    adicionando comentários iniciais

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,34 @@
 var socket = io('http://192.168.0.107:3000');
 
-        function renderMessege(message){
-            $('.messages').append('<div class="message"><strong>'+message.author+'</strong>:'+message.message+'</div>');
-        }
+function renderMessege(message){ //função para renderizar as mensagens na tela com jquery
+    $('.messages').append('<div class="message"><strong>'+message.author+'</strong>:'+message.message+'</div>');
+}
 
-        socket.on('previousMessages', function(messages){
-            for (messages of messages){
-                renderMessege(messages)
-            }
-        });
+socket.on('previousMessages', function(messages){ //"ouvindo" evento emitido pelo back-end e percorrendo array de mensagens 
+    for (messages of messages){
+        renderMessege(messages)
+    }
+});
 
-        socket.on('receivedMessage', function(message){
-            renderMessege(message);
-        })
+socket.on('receivedMessage', function(message){ //recebendo o objeto enviado pelo back-end
+    renderMessege(message);
+})
 
+$('#chat').submit(function(event){ //verificando quando o chat for submitado
+    event.preventDefault(); //função para não enviar o formulário
 
-        $('#chat').submit(function(event){
-            event.preventDefault();
+    var author = $('input[name=username]').val(); //recebendo o nome do autor
+    var message = $('input[name=message]').val(); //recebendo a mensagem
 
-            var author = $('input[name=username]').val();
-            var message = $('input[name=message]').val();
+    if(author.length && message.length){ //verificando se existe username ou mensagem informados
+        var messageObject = { //enviando um objeto por web socket, não nos limitando a strings ou int, podemos enviar arrays  etc.
+            author: author,
+            message: message,
+        };
+        renderMessege(messageObject) //chamando a função assim que enviarmos uma nova mensagem
+        socket.emit('sendMessage', messageObject) //enviando o evento da mensagem
 
-            if(author.length && message.length){
-                var messageObject = {
-                    author: author,
-                    message: message,
-                };
-                renderMessege(messageObject)
-                socket.emit('sendMessage', messageObject)
-
-                message = document.getElementById('message');
-                message.value = "";
-            }
-        });
+        message = document.getElementById('message'); //função para retornar a mensagem por um ID específico.
+        message.value = "";
+    }
+});
--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const express = require('express'); //tratativa para rota (arquivo estático)
 const path = require('path');
-const { Socket } = require('socket.io');
+const { Socket } = require('socket.io'); //meio de comunicação entre front-end e back-end
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require('http').createServer(app); //definindo protocolo http
+const io = require('socket.io')(server); //definindo protocolo socket, que chamará a função server
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public'))); //pasta onde ficarão os arquivos públicos do projeto
+app.set('views', path.join(__dirname, 'public')); //definindo pasta onde ficarão as views
+app.engine('html', require('ejs').renderFile); //definindo a engine como html
+app.set('view engine', 'html'); //agora podemos utilizar html nas views do porjeto
 
 app.use('/', (req,res) =>{
-    res.render('index.html');
+    res.render('index.html'); //quando acessar o endereço do servidor padrão, arquivo index.html vai renderizar
 });
 
-let messages = [];
+let messages = []; //array para armazenar todas as mensagens recebidos, já que não há BD
 
-io.on('connection', socket =>{
+//Definindo a forma de conexão do usuário com o servidor de socket
+io.on('connection', socket =>{ //toda vez que um novo cliente se conectar, recebemos o socket
     console.log(`Socket conectado: ${socket.id}`);
 
-    socket.emit('previousMessages', messages);
+    /*enviando todas as mensagens anteriores assim que o socket conectar na aplicação
+    assim, as mensagens apenas serão perdidas se o servidor for reiniciado*/
+    socket.emit('previousMessages', messages); 
 
-    socket.on('sendMessage', data =>{
-        messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
+    socket.on('sendMessage', data =>{ //"ouvir" evento e dados do front-end
+        messages.push(data); //armazenando as mensagens no array
+        socket.broadcast.emit('receivedMessage', data); //enviando mensagem para todos conectados na aplicação
     });
 });
 
-server.listen(3000);
+server.listen(3000); //definindo porta 
