aba801db982bb49c8ab2780308a797066e7abba5
Davi Perdigão <davi.perdigao@interno.grupoalfa.local>
2022-11-16 20:09:34 -0300

    Ajuste texto terminal

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ let messages = []; //array para armazenar todas as mensagens recebidos, já que 
 //Definindo a forma de conexão do usuário com o servidor de socket
 io.on('connection', socket =>{ //toda vez que um novo cliente se conectar, recebemos o socket
     const ip = socket.handshake.address // atribui para a variavel qual é ip da maquina que esta acessando esse socket 
-    console.log(`Socket conectado: ${socket.id}`, `\nIp do Client: ${ip.replace('::ffff:','')}`);
+    console.log(`Socket conectado: ${socket.id}`, ` - Ip do Client: ${ip.replace('::ffff:','')}`);
 
     /*enviando todas as mensagens anteriores assim que o socket conectar na aplicação
     assim, as mensagens apenas serão perdidas se o servidor for reiniciado*/
